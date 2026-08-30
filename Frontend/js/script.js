f3b99d250a283d25fd6c3e0ecced0b177154f69b
jhon-DMM <jhonsitomondragon12@gmail.com>
2026-08-29 21:24:31 -0500

    estructura inicial css de la pagina

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -3,7 +3,9 @@ const contactModal = document.querySelector("#contact-modal");
 const closeButton = document.querySelector("#close-button");
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
-
+const aboutButton = document.querySelector("#about-button");
+const aboutModal = document.querySelector("#about-modal");
+const aboutCloseButton = document.querySelector("#about-close-button");
 
 
 contactButton.addEventListener("click", function () {
@@ -28,4 +30,24 @@ document.addEventListener("keydown", function (event) {
 
 menuButton.addEventListener("click", function () {
     menu.classList.toggle("active");
+});
+
+aboutButton.addEventListener("click", function () {
+    aboutModal.classList.add("active");
+});
+
+aboutCloseButton.addEventListener("click", function () {
+    aboutModal.classList.remove("active");
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+        aboutModal.classList.remove("active");
+    }
+});
+
+aboutModal.addEventListener("click", function (event) {
+    if (event.target === aboutModal) {
+        aboutModal.classList.remove("active");
+    }
 });
